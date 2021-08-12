8d6b254b30897d2aa4aac0998e74782ecc601a02
Emojigit <55009343+Emojigit@users.noreply.github.com>
2021-08-12 08:01:34 +0800

    Contents

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # digibanner
+Take `signs_api`'s banner to digiline!
+## Code License
+    Copyright (C) 2021 Cato Yiu and other contributers
+    
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+P.S: for a full list of contributer list, see:
+* https://github.com/pyrollo/digiterms/commits/master/nodes.lua
+* https://github.com/pyrollo/display_modpack/commits/master/signs_road/nodes.lua
+* https://github.com/C-C-Minetest-Server/digibanner/commits/main
+* https://github.com/pyrollo/display_modpack/commits/master/signs_api/init.lua
---
+++ init.lua
@@ -0,0 +1,47 @@
+--[[
+This fle is a part of digibanner.
+
+Copyright (C) 2021 Cato Yiu and other contributers
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+]]--
+
+digiterms.register_monitor("digibanner:large_street_sign",{
+  description = "Digiline Banner",
+  paramtype = "light",
+  paramtype2 = "facedir",
+  drawtype = "nodebox",
+  sunlight_propagates = false,
+  node_box = {
+    type = "fixed",
+    fixed = {-(64/16)/2, -(12/16)/2, 0.5,
+      (64/16)/2, (12/16)/2, 0.5 - (1/16)},
+  },
+  groups = {choppy=2, dig_immediate=2, not_blocking_trains=1, display_api=1},
+  sounds = default.node_sound_defaults(),
+  display_entities = {
+    ["digiterms:screen"] = {
+      on_display_update = font_api.on_display_update,
+      depth = 0.5 - display_api.entity_spacing - (1/16),
+      size = { x = 64/16, y = 12/16 },
+      lines = 1, columns = 19,
+      aspect_ratio = 1/2,
+      color = "#000", font_name = "metro",
+    },
+	},
+  tiles = { "signs_road_sides.png", "signs_road_sides.png",
+    "signs_road_sides.png", "signs_road_sides.png",
+    "signs_road_sides.png", "signs_road_large_white.png" },
+  inventory_image = "signs_road_white.png",
+})
